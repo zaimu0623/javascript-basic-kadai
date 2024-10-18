051f7736b6cf1e39bcbe126ba7787cddce71a7d0
zaimu0623 <seibu.saiki0316@gmail.com>
2024-10-18 13:33:16 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+let num=15;
+
+console.log(num);
+
+if(num % 5 === 0 && num % 5 === 0){
+  console.log('3と5の倍数です');
+}
+
+else if(num % 3 === 0){
+  console.log('3の倍数です');
+}
+
+else if(num % 5 === 0){
+  console.log('5の倍数です');
+}
+
+else{
+  console.log(num);
+}
