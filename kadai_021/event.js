aee9c284276cd6d3f73054004baa84c461357967
zaimu0623 <seibu.saiki0316@gmail.com>
2024-11-01 11:24:43 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,10 +1,8 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
-setTimeout(()=> {
-
 btn.addEventListener('click', () => {
+  setTimeout(()=> {
   text.textContent = 'ボタンをクリックしました';
-});
-
 },2000);
+});
